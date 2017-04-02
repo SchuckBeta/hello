a29ef460cdfa9f3b9bb6b4c8ee80b929593b43a2
SchuckBate <13580366759@163.com>
2017-04-02 18:01:05 +0800

    go hello

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+        "fmt"
+        "log"
+        "net/http"
+        "io/ioutil"
 )
 
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!!!"))
+
+type Page struct {
+    Title string
+    Body  []byte
 }
 
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!!!"))
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+
 func main() {
-	fmt.Println("hello world")
-	http.HandleFunc("/", HelloWorld)
-	log.Fatal(http.ListenAndServe(":80", nil))
+        p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+        p1.save()
+        p2, _ := loadPage("TestPage")
+        fmt.Println(string(p2.Body))
+
+
+        fmt.Println("hello world")
+        http.HandleFunc("/", HelloWorld)
+        log.Fatal(http.ListenAndServe(":80", nil))
 }
